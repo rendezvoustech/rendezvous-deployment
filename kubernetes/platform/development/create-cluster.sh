a516a6679307be85f4cc242115993cb8279b290b
Mikkel Arentoft <mikkel@mikadocs.com>
2022-12-07 18:28:34 +0100

    Added authentication / authorization

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -8,11 +8,31 @@ echo "\n🔌 Enabling NGINX Ingress Controller...\n"
 
 minikube addons enable ingress --profile rendezvous
 
-sleep 15
+sleep 30
 
-echo "\n📦 Deploying platform services..."
+echo "\n📦 Deploying Keycloak..."
 
-kubectl apply -f services
+kubectl apply -f services/keycloak-config.yml
+kubectl apply -f services/keycloak.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Keycloak to be deployed..."
+
+while [ $(kubectl get pod -l app=rendezvous-keycloak | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Keycloak to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=rendezvous-keycloak \
+  --timeout=300s
+
+echo "\n📦 Deploying PostgreSQL..."
+
+kubectl apply -f services/postgresql.yml
 
 sleep 5
 
@@ -29,6 +49,12 @@ kubectl wait \
   --selector=db=rendezvous-postgres \
   --timeout=180s
 
+echo "\n📦 Deploying Redis..."
+
+kubectl apply -f services/redis.yml
+
+sleep 5
+
 echo "\n⌛ Waiting for Redis to be deployed..."
 
 while [ $(kubectl get pod -l db=rendezvous-redis | wc -l) -eq 0 ] ; do
@@ -42,6 +68,12 @@ kubectl wait \
   --selector=db=rendezvous-redis \
   --timeout=180s
 
+echo "\n📦 Deploying RabbitMQ..."
+
+kubectl apply -f services/rabbitmq.yml
+
+sleep 5
+
 echo "\n⌛ Waiting for RabbitMQ to be deployed..."
 
 while [ $(kubectl get pod -l db=rendezvous-rabbitmq | wc -l) -eq 0 ] ; do
@@ -53,6 +85,25 @@ echo "\n⌛ Waiting for RabbitMQ to be ready..."
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=rendezvous-rabbitmq \
+  --timeout=180s
+
+echo "\n📦 Deploying Rendezvous UI..."
+
+kubectl apply -f services/rendezvous-ui.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Rendezvous UI to be deployed..."
+
+while [ $(kubectl get pod -l app=rendezvous-ui | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Rendezvous UI to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=rendezvous-ui \
   --timeout=180s
 
 echo "\n⛵ Happy Sailing!\n"
